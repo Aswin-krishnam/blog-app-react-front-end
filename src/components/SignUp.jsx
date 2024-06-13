de8c6ddbf902ab5a43a619d7758e3e79784aee97
Aswin-krishnam <aswinkrishnam16@gmail.com>
2024-06-13 13:49:11 +0530

    Sign Up done

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+import axios from 'axios'
+
+const SignUp = () => {
+    const [data,setData]=useState(
+        {
+            "name":"",
+            "email":"",
+            "password":""
+
+
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8080/signup",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("User added Successfully")
+                    
+                } else {
+                    alert("Error")
+                }   
+            }
+        ).catch(
+            (error)=>{
+                console.log(error.message)
+                alert(error.message)
+            }
+        )
+    }
+  return (
+    <div>
+        <Navbar/>
+        <center>
+            <h3>Signup</h3>
+        </center>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">User Name</label>
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="text" className="form-control" name='password' value={data.password} onChange={inputHandler} />
+
+                        </div>
+                        
+                
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                            <button className="btn btn-success" onClick={readValue}>Add User</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default SignUp
